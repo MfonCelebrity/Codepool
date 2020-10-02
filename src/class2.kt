dae48eceb9fa28b9d5c35fedd15da9b722d375fb
MfonCelebrity <mfon.ubom@gmail.com>
2020-10-02 17:33:53 +0200

    commit

---
+++ src/class2.kt
@@ -0,0 +1,13 @@
+fun main(){
+var serrrr = 4
+    var vserrrr2 = 6
+    for (i in 2..5)
+    println(i)
+
+
+
+
+}
+
+
+
---
+++ src/class3.kt
@@ -0,0 +1,19 @@
+fun main() {
+}
+class Tutorials {
+
+    private var mfon: String = "33"
+
+    fun subject() {
+        print("i am great")
+    }
+
+    fun main(args: Array<String>) {
+
+        val school = Tutorials()
+        school.subject()
+
+    }
+
+
+}
